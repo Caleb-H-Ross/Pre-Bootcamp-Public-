cc502aa9a7c4081d356d7862392ae8fb09e8d343
Caleb-H-Ross <103952500+Caleb-H-Ross@users.noreply.github.com>
2022-05-25 19:31:03 -0400

    Add files via upload

---
+++ Yellow Belt Exam/script.js
@@ -0,0 +1,11 @@
+function changeImage(element) {
+    element.src = "images/succulents-2.jpg"
+}
+
+function changeImageBack(element) {
+    element.src = "images/succulents-1.jpg"
+}
+
+function remove(element) {
+    element.remove()
+}
